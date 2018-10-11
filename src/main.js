e2f28867b4484581bdfc0665eef3ac3f2402b77b
Stephen Lineker-Miller <slinekermiller@gmail.com>
2018-10-11 22:44:37 +0100

    Added all the basic template code for deploying and interacting with Firebase.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue';
+import vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
+import App from '@/App.vue';
+import router from '@/router';
+
+Vue.use(vuetify);
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount("#app");
+	router,
+	render: h => h(App),
+}).$mount('#app');
