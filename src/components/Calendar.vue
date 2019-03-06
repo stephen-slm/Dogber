4557d81b947153e0cc7402c9a0e1bc38a30cc41e
Stephen Lineker-Miller <slinekermiller@gmail.com>
2019-03-06 22:22:49 +0000

    Templated the home page.
    * Created a generic panel that can take some placement text, colors and a slot for the right side.
    
    * Home page now uses the generic panel for core blocks.
    
    * Template Calendar panel

---
+++ src/components/Calendar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-layout wrap>
+    <v-flex xs12 class="mb-3">
+      <v-sheet height="500">
+        <v-calendar ref="calendar" v-model="start" :type="type" :end="end" color="primary"></v-calendar>
+      </v-sheet>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    type: 'month',
+    start: new Date().toDateString(),
+    end: '',
+    typeOptions: [
+      { text: 'Day', value: 'day' },
+      { text: '4 Day', value: '4day' },
+      { text: 'Week', value: 'week' },
+      { text: 'Month', value: 'month' },
+      { text: 'Custom Daily', value: 'custom-daily' },
+      { text: 'Custom Weekly', value: 'custom-weekly' }
+    ]
+  })
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,43 @@
 <template>
-  <div class="home">
-    <div>
-      <img class="avatar" alt="Peoples Image" :src="image" />
-    </div>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 md6 class="box-spacing">
+        <GenericPanel topText="Welcome to Dogber" bottomText="Pending walk requests">12</GenericPanel>
+      </v-flex>
 
-    <HelloWorld :name="name" />
-  </div>
+      <v-flex xs12 sm6 md6 class="box-spacing">
+        <GenericPanel topText="Activities" bottomText="Confirmed/Future Walks">37</GenericPanel>
+      </v-flex>
+
+      <v-flex xs12 sm6 md3 class="box-spacing">
+        <GenericPanel bottomText="Miles Walked" topTextColor="blue" topText="37.6K">
+          <v-icon>directions_walk</v-icon>
+        </GenericPanel>
+      </v-flex>
+
+      <v-flex xs12 sm6 md3 class="box-spacing">
+        <GenericPanel bottomText="My 5 Star Rating" topTextColor="red" topText="4.75">
+          <v-icon>star</v-icon>
+        </GenericPanel>
+      </v-flex>
+
+      <v-flex xs12 sm6 md3 class="box-spacing">
+        <GenericPanel bottomText="Completed Walks" topTextColor="green" topText="107">
+          <v-icon>check</v-icon>
+        </GenericPanel>
+      </v-flex>
+
+      <v-flex xs12 sm6 md3 class="box-spacing">
+        <GenericPanel bottomText="Available" topTextColor="orange" topText="£48.64">
+          <v-icon>credit_card</v-icon>
+        </GenericPanel>
+      </v-flex>
+
+      <v-flex xs12 class="calendar-wrapper">
+        <Calendar/>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -13,6 +45,8 @@ import _ from 'lodash';
 
 import firebaseWrapper from '@/lib/firebaseWrapper';
 import HelloWorld from '@/components/HelloWorld.vue';
+import GenericPanel from '@/components/GenericPanel.vue';
+import Calendar from '@/components/Calendar.vue';
 
 export default {
   name: 'Home',
@@ -41,7 +75,9 @@ export default {
   methods: {},
 
   components: {
-    HelloWorld
+    HelloWorld,
+    GenericPanel,
+    Calendar
   }
 };
 </script>
@@ -52,5 +88,13 @@ export default {
   width: 50px;
   height: 50px;
   border-radius: 50%;
+}
+
+.calendar-wrapper {
+  margin-top: 25px;
+}
+
+.box-spacing {
+  margin-bottom: 5px;
 }
 </style>
