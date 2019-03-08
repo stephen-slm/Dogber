e33fceb2b48af3e939d30b8f14f3ae0803f806dc
Stephen Lineker-Miller <slinekermiller@gmail.com>
2019-03-08 14:19:23 +0000

    Properties on the home page.
    * The home page properties for balance, current rating, completed walks are now being gathered and displayed.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,17 @@
   <v-container grid-list-md text-xs-center>
     <v-layout row wrap>
       <v-flex xs12 sm6 md6 class="box-spacing">
-        <GenericPanel top-text="Welcome to Dogber" bottom-text="Pending walk requests">
-          {{ pendingWalks }}
-        </GenericPanel>
+        <GenericPanel
+          top-text="Welcome to Dogber"
+          bottom-text="Pending walk requests"
+        >{{ pendingWalks }}</GenericPanel>
       </v-flex>
 
       <v-flex xs12 sm6 md6 class="box-spacing">
-        <GenericPanel top-text="Activities" bottom-text="Confirmed/Future Walks">
-          {{ confirmedWalks }}
-        </GenericPanel>
+        <GenericPanel
+          top-text="Activities"
+          bottom-text="Confirmed/Future Walks"
+        >{{ confirmedWalks }}</GenericPanel>
       </v-flex>
 
       <v-flex xs12 sm6 md3 class="box-spacing">
@@ -26,7 +28,11 @@
       </v-flex>
 
       <v-flex xs12 sm6 md3 class="box-spacing">
-        <GenericPanel bottom-text="Completed Walks" top-text-color="green" :top-text="completedWalks">
+        <GenericPanel
+          bottom-text="Completed Walks"
+          top-text-color="green"
+          :top-text="completedWalks"
+        >
           <v-icon>check</v-icon>
         </GenericPanel>
       </v-flex>
@@ -38,7 +44,7 @@
       </v-flex>
 
       <v-flex xs12 class="calendar-wrapper">
-        <Calendar />
+        <Calendar/>
       </v-flex>
     </v-layout>
   </v-container>
@@ -65,9 +71,25 @@ export default {
     };
   },
 
-  created: function() {
+  created: async function() {
     // get the currenlty authenticated user.
     const user = firebaseWrapper.getCurrentUser();
+    const profile = await firebaseWrapper.getProfile();
+
+    if (!_.isNil(profile) && !_.isNil(user)) {
+      console.log(profile, user);
+
+      this.name = profile.name;
+
+      // set all the required data fields on the home page for displaying.
+      this.currentRating = profile.walk.rating / profile.walk.completed;
+      this.completedWalks = profile.walk.completed;
+      this.availableIncome = `£${profile.walk.balance}`;
+
+      // when a user has 0 rating and 0 complted walks then there value is going to be NaN, if this
+      // is true then we are just just to set the value back to 0
+      if (_.isNaN(this.currentRating)) this.currentRating = 0;
+    }
 
     // if we are authenticated and the user object exists, set the name and image from the
     // authenticated object.
