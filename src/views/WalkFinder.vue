3174b572525716ec7d32516476498b466f801489
up806612 <up806612@myport.ac.uk>
2019-03-10 19:00:19 +0000

    Update WalkFinder.vue
    Linting Fixes

--- src/views/WalkFinder.vue
+++ src/views/WalkFinder.vue
@@ -1,51 +1,39 @@
 <template>
   <div id="walkFinder">
-  <v-app id="inspire">
-    <v-container grid-list-md text-xs-center>
-      <v-layout row wrap>
-        <v-flex xs12>
-          <v-card>
-            <v-card-text class="px-0">Walk Finder</v-card-text>
-          </v-card>
-        </v-flex>
-        <v-flex xs12 sm3>
-            <v-overflow-btn
-            :items="dropdown_distanceArea"
-            label="Distance Area"
-          ></v-overflow-btn>
-        </v-flex>
-        <v-flex xs12 sm3>
-          <v-overflow-btn
-            :items="dropdown_ratings"
-            label="Ratings"
-          ></v-overflow-btn>
-         </v-flex>
-        <v-flex xs12 sm3>
-          <v-overflow-btn
-            :items="dropdown_availability"
-            label="Availability"
-          ></v-overflow-btn>
-        </v-flex>
-        <v-flex xs12 sm3>
-          <v-overflow-btn
-            :items="dropdown_priceRange"
-            label="Price Range"
-          ></v-overflow-btn>
-        </v-flex>
-        <v-flex xs12>
-          <v-card>
-            <v-card-text class="px-0">
-              Name: {{ firstName }} {{ lastName }}    Rating: {{ rating }}/10<br />
-              Price Range: £{{ priceMin }} - £{{ priceMax }} (/h)    Distance: {{ distance }} Miles
-            </v-card-text>
-            <v-btn>Profile</v-btn>
-            <v-btn>Check Availability</v-btn>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </v-app>
-</div>
+    <v-app id="inspire">
+      <v-container grid-list-md text-xs-center>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-card>
+              <v-card-text class="px-0">Walk Finder</v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex xs12 sm3>
+            <v-overflow-btn :items="dropdown_distanceArea" label="Distance Area"></v-overflow-btn>
+          </v-flex>
+          <v-flex xs12 sm3>
+            <v-overflow-btn :items="dropdown_ratings" label="Ratings"></v-overflow-btn>
+          </v-flex>
+          <v-flex xs12 sm3>
+            <v-overflow-btn :items="dropdown_availability" label="Availability"></v-overflow-btn>
+          </v-flex>
+          <v-flex xs12 sm3>
+            <v-overflow-btn :items="dropdown_priceRange" label="Price Range"></v-overflow-btn>
+          </v-flex>
+          <v-flex xs12>
+            <v-card>
+              <v-card-text class="px-0">
+                Name: {{ firstName }} {{ lastName }} Rating: {{ rating }}/10<br />
+                Price Range: £{{ priceMin }} - £{{ priceMax }} (/h) Distance: {{ distance }} Miles
+              </v-card-text>
+              <v-btn>Profile</v-btn>
+              <v-btn>Check Availability</v-btn>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-app>
+  </div>
 </template>
 
 <script>
