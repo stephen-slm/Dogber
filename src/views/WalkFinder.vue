70d419523ce41edf46ddc1fe55142fe5aaa071e2
up806612 <up806612@myport.ac.uk>
2019-03-10 18:59:00 +0000

    Update WalkFinder.vue
    * Correcting an error with <br />.

--- src/views/WalkFinder.vue
+++ src/views/WalkFinder.vue
@@ -35,7 +35,7 @@
         <v-flex xs12>
           <v-card>
             <v-card-text class="px-0">
-              Name: {{ firstName }} {{ lastName }}    Rating: {{ rating }}/10</br>
+              Name: {{ firstName }} {{ lastName }}    Rating: {{ rating }}/10<br />
               Price Range: £{{ priceMin }} - £{{ priceMax }} (/h)    Distance: {{ distance }} Miles
             </v-card-text>
             <v-btn>Profile</v-btn>
@@ -60,10 +60,6 @@ export default {
       priceMax: 7,
       distance: 0
     };
-    [5, 10, 15, 20, 25, 30, 40, 50, 75, 100];
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    ["TBA"];
-    ["TBA"]
   }
 };
 </script>
