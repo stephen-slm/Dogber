615c04a90a0c0921d7b22c81b71968d2a4867465
up806612 <up806612@myport.ac.uk>
2019-03-10 18:48:26 +0000

    Update WalkFinder.vue
    * Created Template for vue WalkFinder.
    * Variable Templates within vue data.

--- src/views/WalkFinder.vue
+++ src/views/WalkFinder.vue
@@ -1,9 +1,69 @@
 <template>
-  <div>walkfinder</div>
+  <div id="walkFinder">
+  <v-app id="inspire">
+    <v-container grid-list-md text-xs-center>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-card>
+            <v-card-text class="px-0">Walk Finder</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs12 sm3>
+            <v-overflow-btn
+            :items="dropdown_distanceArea"
+            label="Distance Area"
+          ></v-overflow-btn>
+        </v-flex>
+        <v-flex xs12 sm3>
+          <v-overflow-btn
+            :items="dropdown_ratings"
+            label="Ratings"
+          ></v-overflow-btn>
+         </v-flex>
+        <v-flex xs12 sm3>
+          <v-overflow-btn
+            :items="dropdown_availability"
+            label="Availability"
+          ></v-overflow-btn>
+        </v-flex>
+        <v-flex xs12 sm3>
+          <v-overflow-btn
+            :items="dropdown_priceRange"
+            label="Price Range"
+          ></v-overflow-btn>
+        </v-flex>
+        <v-flex xs12>
+          <v-card>
+            <v-card-text class="px-0">
+              Name: {{ firstName }} {{ lastName }}    Rating: {{ rating }}/10</br>
+              Price Range: £{{ priceMin }} - £{{ priceMax }} (/h)    Distance: {{ distance }} Miles
+            </v-card-text>
+            <v-btn>Profile</v-btn>
+            <v-btn>Check Availability</v-btn>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'WalkFinder'
+  name: 'WalkFinder',
+  data: function() {
+    return {
+      firstName: 'Mark',
+      lastName: 'Anthony',
+      rating: 8,
+      priceMin: 3,
+      priceMax: 7,
+      distance: 0
+    };
+    dropdown_distanceArea: [5, 10, 15, 20, 25, 30, 40, 50, 75, 100];
+    dropdown_ratings: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    dropdown_availability: ["TBA"];
+    dropdown_priceRange: ["TBA"]
+  }
 };
 </script>
