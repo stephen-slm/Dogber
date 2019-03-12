e5368411ee0e4ca18a63bc5b10e88cb53b8a1c94
Stephen Lineker-Miller <slinekermiller@gmail.com>
2019-03-12 09:26:21 +0000

    Walk finder
    * Users are now gathered and displayed on the walk finder page for the actually locating a walker.

--- src/views/WalkFinder.vue
+++ src/views/WalkFinder.vue
@@ -5,7 +5,7 @@
         <v-layout row wrap>
           <v-flex xs12>
             <v-card>
-              <v-card-title primary class="title">Walk Finder</v-card-title>
+              <v-card-title primary class="subheading text-sm-left">Walk Finder</v-card-title>
             </v-card>
           </v-flex>
           <v-flex xs12 sm3>
@@ -21,12 +21,7 @@
             <v-select :items="dropdown_priceRange" label="Price Range"></v-select>
           </v-flex>
           <v-flex xs12>
-            <WalkFinderResult
-              v-for="item in [1, 2, 3, 4, 5]"
-              :key="item"
-              class="walk-result"
-              :id="localUserId"
-            />
+            <WalkFinderResult v-for="item in userKeys" :key="item" class="walk-result" :id="item"/>
           </v-flex>
         </v-layout>
       </v-container>
@@ -35,6 +30,7 @@
 </template>
 
 <script>
+import _ from 'lodash';
 import WalkFinderResult from '@/components/WalkFinderResult.vue';
 import firebaseWrapper from '@/lib/firebaseWrapper';
 
@@ -46,8 +42,18 @@ export default {
       dropdown_distanceArea: ['Portsmouth', 'London', 'Brighton'],
       dropdown_ratings: ['0 - 1', '1 - 2', '2 - 3', '3 - 4', '4 - 5'],
       dropdown_availability: ['now', 'later'],
-      dropdown_priceRange: ['0 - 5', '5 - 10', '10 - 20', '20+']
+      dropdown_priceRange: ['0 - 5', '5 - 10', '10 - 20', '20+'],
+      userKeys: []
     };
+  },
+
+  created: async function() {
+    // for preview information, just generating the displaying data for the given user.
+    const usersReference = await firebaseWrapper.database.ref('users').once('value');
+    const users = usersReference.val();
+
+    this.userKeys = _.map(users, (value, index) => index);
+    console.log(this.userKeys);
   },
 
   components: {
