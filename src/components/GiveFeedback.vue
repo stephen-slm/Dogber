28189c560b3b6ff2f60cb44a9ca6134731f63a5f
Stephen Lineker-Miller <slinekermiller@gmail.com>
2019-03-17 17:12:57 +0000

    Give feeback testing
    - Test coverage of the input fields
    - Test coverage of submitting, canceling.

--- src/components/GiveFeedback.vue
+++ src/components/GiveFeedback.vue
@@ -24,8 +24,8 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click="showFeedbackLocal = false">Close</v-btn>
-          <v-btn color="blue darken-1" flat @click="submitLocal()">Save</v-btn>
+          <v-btn id="feedcancel" color="blue darken-1" flat @click="showFeedbackLocal = false">Close</v-btn>
+          <v-btn id="feedsubmit" color="blue darken-1" flat @click="submitLocal()">Save</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -93,5 +93,4 @@ export default {
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
