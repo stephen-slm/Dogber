5a9bb6d4de2cfd0b72a7ed895d271ae00ec03b90
Stephen Lineker-Miller <slinekermiller@gmail.com>
2019-03-17 11:51:46 +0000

    Dark Mode
    - Dark mode trigger is now based off the local storage.
    
    - Settings page now contains passive dark mode toggle.
    
    - Updated Navigation notifications icon to allow for dark mode support.
    
    - Updated WalkFinderResult button to allow for dark mode support.

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,9 +1,43 @@
 <template>
-  <div>settings</div>
+  <v-container class="container">
+    <v-card class="settings-container">
+      <form>
+        <div class="text-sm-left gray lighten-1">Passive</div>
+        <v-divider />
+        <v-switch v-model="dark" label="Dark Mode"></v-switch>
+      </form>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'Settings'
+  name: 'Settings',
+
+  data: function() {
+    return {
+      dark: false
+    };
+  },
+
+  mounted: function() {
+    if (localStorage.dark) this.dark = localStorage.dark === 'true' ? true : false;
+  },
+
+  watch: {
+    dark: function(newValue) {
+      this.$emit('set-dark', newValue);
+    }
+  }
 };
 </script>
+
+<style scoped>
+.container {
+  max-width: 500px;
+}
+
+.settings-container {
+  padding: 25px;
+}
+</style>
