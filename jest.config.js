8fbb9fa6aaa4bf2cb1f5cbea9891a6176844014d
Stephen Lineker-Miller <slinekermiller@gmail.com>
2019-03-17 14:01:50 +0000

    Firebase Deployment
    - Firebase deployment now setup for master
    
    - Jest now checks for running async processes.

--- jest.config.js
+++ jest.config.js
@@ -12,5 +12,7 @@ module.exports = {
   testMatch: ['**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'],
   testURL: 'http://localhost/',
   collectCoverage: false,
+  verbose: true,
+  detectOpenHandles: true,
   setupTestFrameworkScriptFile: './tests/unit/jest-setup.js'
 };
