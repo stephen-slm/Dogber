e8fb2add1a1c5931f6174261afbee9deb37f44a8
up806612 <up806612@myport.ac.uk>
2019-03-20 21:09:49 +0000

    Update Walks.vue
    * Removed Price and Walk Distance dropdown.
    * Dropdown for Date is changed to show either Ascending or Descending order.

--- src/views/Walks.vue
+++ src/views/Walks.vue
@@ -14,12 +14,6 @@
           <v-flex xs12 sm3>
             <v-select :items="dropdown_date" label="Date"></v-select>
           </v-flex>
-          <v-flex xs12 sm3>
-            <v-select :items="dropdown_price" label="Price"></v-select>
-          </v-flex>
-          <v-flex xs12 sm3>
-            <v-select :items="dropdown_walkDistance" label="Walk Distance"></v-select>
-          </v-flex>
           <v-flex xs12>
             <WalkHistory v-for="item in userKeys" :key="item" class="walk-history" :id="item" />
           </v-flex>
@@ -39,9 +33,7 @@ export default {
     return {
       localUserId: firebaseWrapper.getUid(),
       dropdown_walkCompleted: ['Yes', 'No'],
-      dropdown_date: ['All', 'Last 24 Hours', 'Last 7 Days', 'Last 14 Days', 'Last 30 Days', 'Last 60 Days'],
-      dropdown_price: ['0 - 5', '5 - 10', '10 - 20', '20+'],
-      dropdown_walkDistance: ['0 - 1', '1 - 3', '2 - 5', '5 - 10', '10+']
+      dropdown_date: ['Ascending', 'Descending']
     };
   },
 
