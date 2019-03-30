4f9b3f683529c9243bf31e86b535ac760424d5ac
Stephen Lineker-Miller <slinekermiller@gmail.com>
2019-03-30 16:34:41 +0000

    walk processes.
    Accept, reject, complete and cancel walk functions.
    Fixed navigation related error that does not change the page when navigating to the same page with a new id.

--- src/views/Walk.vue
+++ src/views/Walk.vue
@@ -66,17 +66,25 @@
             <v-flex xs4>
               <ActionWithNotes
                 :disabled="isOwner"
-                v-if="walk.status != null && this.walk.status === walkStats.PENDING"
+                v-if="walk.status != null && walk.status === walkStats.PENDING"
                 title="Accepting"
                 button-text="Accept"
+                :submit="acceptWalk"
               />
             </v-flex>
             <v-flex xs4>
               <ActionWithNotes
                 :disabled="isOwner"
-                v-if="walk.status != null && this.walk.status === walkStats.PENDING"
+                v-if="walk.status != null && !isOwner && walk.status === walkStats.PENDING"
                 title="Rejecting"
                 button-text="Reject"
+                :submit="rejectWalk"
+              />
+              <ActionWithNotes
+                v-if="walk.status != null && walk.status === walkStats.PENDING"
+                title="Cancelling"
+                button-text="Cancel"
+                :submit="cancelWalk"
               />
             </v-flex>
           </v-layout>
@@ -130,10 +138,12 @@
 </template>
 
 <script>
-import firebaseWrapper from '../lib/firebaseWrapper.js';
+import * as _ from 'lodash';
+
 import * as firebaseConstants from '../constants/firebaseConstants.js';
-import DogsGrid from '@/components/DogsGrid.vue';
 import ActionWithNotes from '@/components/ActionWithNotes.vue';
+import firebaseWrapper from '../lib/firebaseWrapper.js';
+import DogsGrid from '@/components/DogsGrid.vue';
 
 export default {
   data: function() {
@@ -178,41 +188,51 @@ export default {
    * display and adjust the page.
    */
   created: async function() {
-    // the local walk id, this is the id that is used to reference the id, this will be used to
-    // gather the walk and the people related to the walk (e.g walker and owner).
-    this.localWalkId = this.$route.params.id || '';
+    await this.initalizePage();
+  },
 
-    // gather the related walker and owners profile including the walker.
-    this.walk = await firebaseWrapper.getWalkByKey(this.localWalkId);
-    this.owner = await firebaseWrapper.getProfile(this.walk.owner);
-    this.walker = await firebaseWrapper.getProfile(this.walk.walker);
-
-    // determine if the current authenticated user is the walker or the owner so we can do simple
-    // little displays indercating who is who.
-    this.isOwner = firebaseWrapper.getUid() === this.walk.owner;
-    this.walkLocation = this.walk.location;
-
-    // lets revserse the history logs so that the related history object is in order when displaying
-    // it for the given user. This will correctly format it for a timeline.
-    this.walk.history = this.walk.history.reverse();
-
-    // lets build up the dogs object for settings.
-    const dogs = {};
-
-    // iterate and perform the requests for gathering the dogs, for of to allow the syncing of the
-    // request process. Then set the dogs onto the object.
-    for (const dog of this.walk.dogs) {
-      dogs[dog] = await firebaseWrapper.getSingleDog(this.walk.owner, dog);
-    }
-
-    // update the current set walks.
-    this.dogs = dogs;
-
-    // setup all related listeners, including the walk (owner and walker).
-    await this.setupWalkListener();
+  watch: {
+    // call again the method if the route changes
+    $route: 'initalizePage'
   },
 
   methods: {
+    initalizePage: async function() {
+      // the local walk id, this is the id that is used to reference the id, this will be used to
+      // gather the walk and the people related to the walk (e.g walker and owner).
+      this.localWalkId = this.$route.params.id || '';
+
+      // gather the related walker and owners profile including the walker.
+      this.walk = await firebaseWrapper.getWalkByKey(this.localWalkId);
+      this.owner = await firebaseWrapper.getProfile(this.walk.owner);
+      this.walker = await firebaseWrapper.getProfile(this.walk.walker);
+
+      // determine if the current authenticated user is the walker or the owner so we can do simple
+      // little displays indercating who is who.
+      this.isOwner = firebaseWrapper.getUid() === this.walk.owner;
+      this.walkLocation = this.walk.location;
+
+      // lets revserse the history logs so that the related history object is in order when displaying
+      // it for the given user. This will correctly format it for a timeline.
+      this.walk.history = _.values(this.walk.history).reverse();
+      this.walk.notes = _.values(this.walk.notes).reverse();
+
+      // lets build up the dogs object for settings.
+      const dogs = {};
+
+      // iterate and perform the requests for gathering the dogs, for of to allow the syncing of the
+      // request process. Then set the dogs onto the object.
+      for (const dog of this.walk.dogs) {
+        dogs[dog] = await firebaseWrapper.getSingleDog(this.walk.owner, dog);
+      }
+
+      // update the current set walks.
+      this.dogs = dogs;
+
+      // setup all related listeners, including the walk (owner and walker).
+      await this.setupWalkListener();
+    },
+
     // if changes occure on the current walk, we want to update the whole object when the changes
     // happen. Keeping all related information live. This will also setup listeners for the owner
     // and the walker, just incase the profiles change at anypoint.
@@ -221,9 +241,14 @@ export default {
       const ownerReference = firebaseWrapper.getProfileReference(this.walk.owner);
       const walkReference = firebaseWrapper.getWalkReferenceByKey(this.localWalkId);
 
-      walkerReference.on('value', (snapshot) => (this.walker = snapshot.val()));
+      walkReference.on('value', (snapshot) => {
+        this.walk = snapshot.val();
+        this.walk.history = _.values(this.walk.history).reverse();
+        this.walk.notes = _.values(this.walk.notes).reverse();
+      });
+
       ownerReference.on('value', (snapshot) => (this.owner = snapshot.val()));
-      walkReference.on('value', (snapshot) => (this.walk = snapshot.val()));
+      walkerReference.on('value', (snapshot) => (this.walker = snapshot.val()));
     },
     // gets the display related text for a given walk message, this is what the user is going to see
     // as the text for a given status. This has to be clean and easy to read.
@@ -258,6 +283,34 @@ export default {
         default:
           return 'black';
       }
+    },
+    // sets and marks the walk as cancelled, no action can take place now since this has gone
+    // through, the other party will be notified about these actions.
+    cancelWalk: async function(notes) {
+      return this.walkAction(firebaseWrapper.cancelWalkRequest.bind(firebaseWrapper), notes);
+    },
+
+    // sets and marks the walk as accepted.
+    acceptWalk: async function(notes) {
+      return this.walkAction(firebaseWrapper.acceptWalkRequest.bind(firebaseWrapper), notes);
+    },
+
+    // sets and marks the walk as rejected.
+    rejectWalk: async function(notes) {
+      return this.walkAction(firebaseWrapper.rejectWalkRequest.bind(firebaseWrapper), notes);
+    },
+
+    // sets and marks the walk as complete.
+    completeWalk: async function(notes) {
+      return this.walkAction(firebaseWrapper.completeWalk.bind(firebaseWrapper), notes);
+    },
+
+    // standard walk request action format, all methods have the same standard param input so this
+    // can be used instead of repeating lots of codes.
+    walkAction: async function(asyncMethod, notes) {
+      const localUserId = this.isOwner ? this.owner.id : this.walker.id;
+      await asyncMethod(localUserId, this.localWalkId, notes === '' ? undefined : notes);
+      return true;
     }
   },
 
